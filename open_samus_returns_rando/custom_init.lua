6565293a4108de28f8ceb03d79086e2463cfcdfb
dyceron <38679103+dyceron@users.noreply.github.com>
2022-12-06 13:34:45 -0500

    Random Start prep (not supported)

--- open_samus_returns_rando/custom_init.lua
+++ open_samus_returns_rando/custom_init.lua
@@ -11,9 +11,10 @@ end
 
 local buff = {}
 
-function Game.StartPrologue(arg1, arg2, arg3, arg4, arg4)
+
+function Init.InitNewGame(arg1, arg2, arg3, arg4, arg4)
     Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
-    Game.LoadScenario("c10_samus", TEMPLATE("starting_scenario"), TEMPLATE("starting_actor"), "", 1)
+    Game.LoadScenario("c10_samus", TEMPLATE("starting_scenario"), TEMPLATE("starting_actor"), "Samus", 1)
 end
 
 Game.LogWarn(0, "Finished modded system/init.lc")
