93d9d3817dac9793b58a54c00fb04b607aaa4d91
dyceron <38679103+dyceron@users.noreply.github.com>
2022-12-07 00:18:12 -0500

    Force all cutscenes to be skippable on a new file

--- open_samus_returns_rando/custom_init.lua
+++ open_samus_returns_rando/custom_init.lua
@@ -11,10 +11,10 @@ end
 
 local buff = {}
 
-
 function Init.InitNewGame(arg1, arg2, arg3, arg4, arg4)
     Game.LogWarn(0, string.format("Will start Game - %s / %s / %s / %s", tostring(arg1), tostring(arg2), tostring(arg3), tostring(arg4)))
     Game.LoadScenario("c10_samus", TEMPLATE("starting_scenario"), TEMPLATE("starting_actor"), "Samus", 1)
 end
 
+Game.SetForceSkipCutscenes(true)
 Game.LogWarn(0, "Finished modded system/init.lc")
