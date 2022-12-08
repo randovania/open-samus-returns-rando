fe69722d2b2af09db1a300c1b55f2236817af462
dyceron <38679103+dyceron@users.noreply.github.com>
2022-12-07 23:35:33 -0500

    Updates and add test

---
+++ tests/test_open_samus_returns_patcher.py
@@ -0,0 +1,15 @@
+from unittest.mock import MagicMock
+
+from open_samus_returns_rando import samus_returns_patcher
+
+
+def test_create_init_copy_exists():
+    editor = MagicMock()
+    editor.does_asset_exists.return_value = True
+
+    # Run
+    samus_returns_patcher.create_init_copy(editor)
+
+    # Assert
+    editor.does_asset_exists.assert_called_once_with("system/scripts/original_init.lc")
+    editor.add_new_asset.assert_not_called()
