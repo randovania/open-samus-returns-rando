531a1f509829cd1fa7f9387374e3cb02a73826c5
dyceron <38679103+dyceron@users.noreply.github.com>
2022-12-09 14:47:30 -0500

    Fix new test

--- tests/test_open_samus_returns_patcher.py
+++ tests/test_open_samus_returns_patcher.py
@@ -9,9 +9,7 @@ def test_create_init_copy_exists():
 
     # Run
     samus_returns_patcher.create_init_copy(editor)
-    samus_returns_patcher.create_scenario_copy(editor)
 
     # Assert
     editor.does_asset_exists.assert_called_once_with("system/scripts/original_init.lc")
-    editor.does_asset_exists.assert_called_once_with("system/scripts/scenario_init.lc")
     editor.add_new_asset.assert_not_called()
