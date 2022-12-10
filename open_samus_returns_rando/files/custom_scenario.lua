4b221e5faea09a73ce1cd799131bdd50ebd6f6f5
MeriKatt <devtanukikitamura@gmail.com>
2022-12-09 21:39:00 -0600

    Pull new changes and re-apply previous changes

--- open_samus_returns_rando/files/custom_scenario.lua
+++ open_samus_returns_rando/files/custom_scenario.lua
@@ -5,7 +5,46 @@ setmetatable(Scenario, {__index = _G})
 setfenv(1, Scenario)
 
 function HideLoadingScreen()
-	Game.SetLoadingScreen(false)
-	loadingscreen.HideLoadingScreen()
-	Game.HUDIdleScreenLeave()
+ Game.SetLoadingScreen(false)
+ loadingscreen.HideLoadingScreen()
+ Game.HUDIdleScreenLeave()
+ if table.getn(CurrentScenario.tHazarous) > 0 then
+  for key, value in pairs(CurrentScenario.tHazarous) do
+   EnableHazarous(key, false)
+  end
+ end
+end
+
+function EnableHazarous(_ARG_0_, _ARG_1_)
+ if CurrentScenario.EnableHazarous ~= nil then
+  CurrentScenario.EnableHazarous(_ARG_0_, _ARG_1_)
+ end
+ if CurrentScenario.OnHazarousPoolDrain ~= nil then
+  CurrentScenario.OnHazarousPoolDrain("")
+ end
+ if CurrentScenario.tHazarous[_ARG_0_] ~= nil then
+  for _FORV_6_, _FORV_7_ in pairs(CurrentScenario.tHazarous[_ARG_0_].Triggers) do
+   if CanaManageHazarousEntity(_ARG_0_, "Triggers", _FORV_7_, _ARG_1_) and Game.GetEntity(_FORV_7_) ~= nil then
+    if _ARG_1_ then
+     Game.GetEntity(_FORV_7_):Disable()
+    else
+     Game.GetEntity(_FORV_7_):Enable()
+    end
+   end
+   end
+   for _FORV_6_, _FORV_7_ in pairs(CurrentScenario.tHazarous[_ARG_0_].SpawnGroups) do
+    if CanaManageHazarousEntity(_ARG_0_, "SpawnGroups", _FORV_7_, _ARG_1_) and Game.GetEntity(_FORV_7_) ~= nil then
+     if _ARG_1_ then
+      Game.GetEntity(_FORV_7_).SPAWNGROUP:DisableSpawnGroup()
+     else
+      Game.GetEntity(_FORV_7_).SPAWNGROUP:EnableSpawnGroup()
+     end
+   end
+  end
+  for _FORV_6_, _FORV_7_ in pairs(CurrentScenario.tHazarous[_ARG_0_].Usables) do
+   if CanaManageHazarousEntity(_ARG_0_, "Usables", _FORV_7_, _ARG_1_) and Game.GetEntity(_FORV_7_) ~= nil then
+    Game.GetEntity(_FORV_7_).USABLE:Activate(not _ARG_1_)
+   end
+  end
+ end
 end
