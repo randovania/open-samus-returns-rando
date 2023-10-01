8997daa6b4eb7fd76f8795b0ffbbb27777c75a08
dyceron <38679103+dyceron@users.noreply.github.com>
2023-10-01 12:35:03 -0400

    Rename copied actor

--- open_samus_returns_rando/custom_pickups.py
+++ open_samus_returns_rando/custom_pickups.py
@@ -7,17 +7,18 @@ def _get_actor(editor: PatcherEditor):
         "layer": "9",
         "actor": "LE_PowerUP_Morphball",
     }
-    new_actor = editor.resolve_actor_reference(actor_to_copy)
-    return new_actor
+    reference_actor = editor.resolve_actor_reference(actor_to_copy)
+    return reference_actor
 
 
 def _create_baby_pickup(editor: PatcherEditor):
     name_of_scenario = "s100_area10"
     scenario_10 = editor.get_scenario(name_of_scenario)
 
-    baby_actor = _get_actor(editor)
+    reference_actor = _get_actor(editor)
+
     editor.copy_actor(
-        name_of_scenario, (-3400.0, 11225.0, 0.0), baby_actor, "LE_Baby_Hatchling", 9
+        name_of_scenario, (-3400.0, 11225.0, 0.0), reference_actor, "LE_Baby_Hatchling", 9,
     )
 
     group_cc22 = scenario_10.get_actor_group("eg_SubArea_collision_camera_022")
