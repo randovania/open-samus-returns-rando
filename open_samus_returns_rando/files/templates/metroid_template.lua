7314d534f2825f45d70fd566c9341cbf1df9402f
dyceron <38679103+dyceron@users.noreply.github.com>
2023-10-09 17:05:35 -0400

    Ensure Gamma spawns regardless starting cc

--- open_samus_returns_rando/files/templates/metroid_template.lua
+++ open_samus_returns_rando/files/templates/metroid_template.lua
@@ -7,10 +7,10 @@ function Metroid.RemoveMetroid(_ARG_0_)
     if spawnGroupName ~= nil then
         -- GUI.LaunchMessage(spawnGroupName, "Metroid.Dummy", "")
 
-        local spawnGroup =  Game.GetEntity(spawnGroupName)
+        local spawnGroup = Game.GetEntity(spawnGroupName)
         if spawnGroup ~= nil and spawnGroup.SPAWNGROUP ~= nil then
             spawnGroup.SPAWNGROUP:DisableSpawnGroup()
-        else 
+        else
             GUI.LaunchMessage("Oops 1" .. spawnGroupName, "Metroid.Dummy", "")
             return
         end
@@ -18,7 +18,7 @@ function Metroid.RemoveMetroid(_ARG_0_)
         Scenario.WriteToBlackboard("entity_" .. spawnGroupName .. "_dead", "b", true)
         Scenario.WriteToBlackboard("entity_" .. spawnGroupName .. "_deaths", "i", 1)
         Scenario.WriteToBlackboard("entity_" .. spawnGroupName .. "_enabled", "b", false)
-        
+
         local count = Game.GetItemAmount(Game.GetPlayerName(), "ITEM_METROID_COUNT") + 1
         Game.SetItemAmount(Game.GetPlayerName(), "ITEM_METROID_COUNT", count)
         Game.IncrementMetroidTotalCount(0)
@@ -27,10 +27,10 @@ function Metroid.RemoveMetroid(_ARG_0_)
 
         local scenario = Scenario.CurrentScenarioID
         if scenario ~= nil and Metroid.Pickups ~= nil and
-            Metroid.Pickups[scenario] ~= nil and 
+            Metroid.Pickups[scenario] ~= nil and
             Metroid.Pickups[scenario][spawnGroupName] ~= nil and
             Metroid.Pickups[scenario][spawnGroupName].OnPickedUp ~= nil then
-                Metroid.Pickups[scenario][spawnGroupName].OnPickedUp()
+            Metroid.Pickups[scenario][spawnGroupName].OnPickedUp()
         end
     else
         GUI.LaunchMessage("Oops 2", "Metroid.Dummy", "")
