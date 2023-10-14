79c2d9bac0ba17893bebcf5cc18626789105b752
dyceron <38679103+dyceron@users.noreply.github.com>
2023-10-14 00:45:33 -0400

    Add unused gui text regarding Spider Boost technique

---
+++ open_samus_returns_rando/misc_patches/text_patches.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import typing
+
+from mercury_engine_data_structures.formats import Txt
+
+if typing.TYPE_CHECKING:
+    from open_samus_returns_rando.patcher_editor import PatcherEditor
+
+# may want to edit all the localization files?
+ALL_TEXT_FILES = {
+    # "eu_dutch.txt",
+    # "eu_english.txt",
+    # "eu_french.txt",
+    # "eu_german.txt",
+    # "eu_italian.txt",
+    # "eu_portuguese.txt",
+    # "eu_spanish.txt",
+    # "japanese.txt",
+    # "mse_english.txt",
+    "us_english.txt",
+    # "us_french.txt",
+    # "us_spanish.txt"
+}
+
+
+def patch_text(editor: PatcherEditor, key: str, value: str):
+    for text_file in ALL_TEXT_FILES:
+        text = editor.get_file(f"system/localization/{text_file}", Txt)
+        text.strings[key] = value
+
+
+def get_text(editor: PatcherEditor, key: str, text_file: str = "us_english.txt") -> str:
+    text = editor.get_file(f"system/localization/{text_file}", Txt)
+    return text.strings[key]
+
+
+def apply_text_patches(editor: PatcherEditor, patches: dict[str, str]):
+    for k, v in patches.items():
+        patch_text(editor, k, v)
+
+
+def patch_pb_status(editor: PatcherEditor):
+    status = f"GUI_STATUS_SPIDERBALL"
+    original = get_text(editor, status)
+    new = original.replace(
+        "activate.",
+        "activate.\nWhile in Spider Ball form, lay a Power Bomb to activate the Spider Boost. It will launch you forward, damaging enemies you make contact with. It can also be used to reach certain items.",
+    )
+    patch_text(editor, status, new)
