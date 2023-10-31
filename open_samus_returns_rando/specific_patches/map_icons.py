fcdc2e9a533936a3ce69e96546e9f8aaf967909f
Thanatos <thanatosstr89@gmail.com>
2023-10-31 19:39:56 +0100

    Use enum and rename

--- open_samus_returns_rando/specific_patches/map_icons.py
+++ open_samus_returns_rando/specific_patches/map_icons.py
@@ -1,11 +1,12 @@
 from mercury_engine_data_structures.formats import Bmsmsd
+from mercury_engine_data_structures.formats.bmsmsd import TileType
 
 from open_samus_returns_rando.constants import ALL_AREAS
 from open_samus_returns_rando.patcher_editor import PatcherEditor
 
 
 def patch_tiles(editor: PatcherEditor):
-    DOORS = {
+    AREA_TO_DOORS = {
         "s000_surface": ["Door004", "Door011"],
         "s010_area1": ["Door002", "Door004", "Door012", "Door016"],
         "s025_area2b": ["Door008"],
@@ -23,6 +24,8 @@ def patch_tiles(editor: PatcherEditor):
         if area != "s110_surfaceb":
             scenario = editor.get_file(f"gui/minimaps/c10_samus/{area}.bmsmsd", Bmsmsd)
             tiles = scenario.raw["tiles"]
+            doors = AREA_TO_DOORS.get(area, [])
+
             for tile_idx in range(len(tiles)):
                 current_tile = tiles[tile_idx]
 
@@ -32,18 +35,18 @@ def patch_tiles(editor: PatcherEditor):
                     icons[0]["icon"].endswith("tank")
                     or icons[0]["icon"].startswith("itemsphere")
                 ):
-                    if current_tile["tile_type"] == "HEAT":
+                    if current_tile["tile_type"] == TileType.HEAT:
                         icons[0]["icon"] = "itemenabledheat"
                     else:
                         icons[0]["icon"] = "itemenabled"
 
-                for door in DOORS.get(area, []):
+                for door in doors:
                     if len(icons) == 1 and icons[0]["actor_name"].startswith(door):
                         icons[0]["clear_condition"] = ""
                         if "left" in icons[0]["icon"]:
                             icons[0]["icon"] = "doorpowerleft"
                         else:
                             icons[0]["icon"] = "doorpowerright"
-                    if len(icons) == 2 and icons[1]["actor_name"].startswith(door) and "left" in icons[1]["icon"]:
+                    elif len(icons) == 2 and icons[1]["actor_name"].startswith(door) and "left" in icons[1]["icon"]:
                         icons[1]["clear_condition"] = ""
                         icons[1]["icon"] = "doorpowerleft"
