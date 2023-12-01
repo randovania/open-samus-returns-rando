2248db788fda832bee911ed3c607c380da144041
dyceron <38679103+dyceron@users.noreply.github.com>
2023-11-30 20:22:53 -0500

    Rename config names to be more readable

--- src/open_samus_returns_rando/specific_patches/tunable_patches.py
+++ src/open_samus_returns_rando/specific_patches/tunable_patches.py
@@ -17,7 +17,7 @@ def _patch_scan_pulse(tunables: Bmtun):
 
 def _reserves_per_tank(tunables: Bmtun, configuration: dict):
     amiibo = tunables.raw["classes"]["Amiibo|CTunableReserveTanks"]["tunables"]
-    amiibo["fLifeTankSize"]["value"] = configuration["fLifeTankSize"]
-    amiibo["fSETankSize"]["value"] = configuration["fSETankSize"]
-    amiibo["fMissileTankSize"]["value"] = configuration["fMissileTankSize"]
-    amiibo["fSuperMissileTankSize"]["value"] = configuration["fSuperMissileTankSize"]
+    amiibo["fLifeTankSize"]["value"] = configuration["life_tank_size"]
+    amiibo["fSETankSize"]["value"] = configuration["aeion_tank_size"]
+    amiibo["fMissileTankSize"]["value"] = configuration["missile_tank_size"]
+    amiibo["fSuperMissileTankSize"]["value"] = configuration["super_missile_tank_size"]
