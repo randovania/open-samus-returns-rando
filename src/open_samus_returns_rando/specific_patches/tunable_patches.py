ca80c17f4805592a2170ae9c92d2819979eccd70
dyceron <38679103+dyceron@users.noreply.github.com>
2023-12-02 08:56:55 -0500

    Support customizable Reserve Tank sizes (#201)
    * Support customizable Reserve Tank sizes
    
    * Add maximum values
    
    * Rename config names to be more readable
    
    * Remove max since max capacity is already respected

--- src/open_samus_returns_rando/specific_patches/tunable_patches.py
+++ src/open_samus_returns_rando/specific_patches/tunable_patches.py
@@ -2,12 +2,22 @@ from mercury_engine_data_structures.formats.bmtun import Bmtun
 from open_samus_returns_rando.patcher_editor import PatcherEditor
 
 
+def patch_tunables(editor: PatcherEditor, configuration: dict):
+    tunables = editor.get_file("system/tunables/tunables.bmtun", Bmtun)
+    _patch_scan_pulse(tunables)
+    if "reserves_per_tank" in configuration:
+        _reserves_per_tank(tunables, configuration)
+
+
 def _patch_scan_pulse(tunables: Bmtun):
     tunables.raw["classes"]["CTunableAbilityScanningPulse"]["tunables"][
         "fConsumptionOnActivation"
     ]["value"] = 0.0
 
 
-def patch_tunables(editor: PatcherEditor):
-    tunables = editor.get_file("system/tunables/tunables.bmtun", Bmtun)
-    _patch_scan_pulse(tunables)
+def _reserves_per_tank(tunables: Bmtun, configuration: dict):
+    amiibo = tunables.raw["classes"]["Amiibo|CTunableReserveTanks"]["tunables"]
+    amiibo["fLifeTankSize"]["value"] = configuration["life_tank_size"]
+    amiibo["fSETankSize"]["value"] = configuration["aeion_tank_size"]
+    amiibo["fMissileTankSize"]["value"] = configuration["missile_tank_size"]
+    amiibo["fSuperMissileTankSize"]["value"] = configuration["super_missile_tank_size"]
