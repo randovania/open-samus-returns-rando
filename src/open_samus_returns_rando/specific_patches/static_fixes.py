0ef824b3875f4a408f8b5f9c963e23aff8723c47
dyceron <dycerontv@gmail.com>
2024-01-17 11:26:46 -0500

    Use factors that balance out each beam and adjust boost values

--- src/open_samus_returns_rando/specific_patches/static_fixes.py
+++ src/open_samus_returns_rando/specific_patches/static_fixes.py
@@ -80,26 +80,29 @@ def shoot_supers_without_missiles(editor: PatcherEditor):
 
 
 def nerf_ridley_fight(editor: PatcherEditor):
-    # Power Beam: 3 (25 * 0.12)
-    # Ice: 10
-    # Wave: 6 (50 * 0.12)
-    # Spazer: 25,2 (210 * 0.12)
-    # Plasma: 36 (default of 300 * 0.12 in tunable of ridley)
+    '''
+    Beams also include previous stage beams and factors (excluding Ice)
+    Power Beam: 3 (25 * 0.12)
+    Ice: 10
+    Wave: 13.98 (50 * 2.33)
+    Spazer: 25.25 (210 * 0.43)
+    Plasma: 36.07 (default is 36 with 300 * 0.12 in tunable of ridley)
+    '''
     ridley_bmsad = editor.get_file("actors/characters/ridley/charclasses/ridley.bmsad", Bmsad)
     ridley_bmsad.components["LIFE"].raw["fields"]["fPowerBeamFactor"]["value"] = 0.12
-    # ridley_bmsad.components["LIFE"].raw["fields"]["fWaveBeamFactor"]["value"] = 1.0
-    # ridley_bmsad.components["LIFE"].raw["fields"]["fSpazerBeamFactor"]["value"] = 1.0
+    ridley_bmsad.components["LIFE"].raw["fields"]["fWaveBeamFactor"]["value"] = 2.33
+    ridley_bmsad.components["LIFE"].raw["fields"]["fSpazerBeamFactor"]["value"] = 0.43
 
     tunables = editor.get_file("system/tunables/tunables.bmtun", Bmtun)
     ridley_tunables = tunables.raw["classes"]["Ridley|CTunableCharClassRidleyAIComponent"]["tunables"]
-    ridley_tunables["fGrabPlasmaBeam"]["value"] = 1.0
-    ridley_tunables["fGrabPlasmaBeamCharge"]["value"] = 1.0
-    ridley_tunables["fGrabPlasmaBeamWeaponBoost"]["value"] = 1.0
-    ridley_tunables["fGrabPlasmaBeamWeaponBoostPhaseDisplacement"]["value"] = 1.0
     ridley_tunables["fPlasmaBeam"]["value"] = 1.0
     ridley_tunables["fPlasmaBeamCharge"]["value"] = 1.0
-    ridley_tunables["fPlasmaBeamWeaponBoost"]["value"] = 1.0
-    ridley_tunables["fPlasmaBeamWeaponBoostPhaseDisplacement"]["value"] = 1.0
+    ridley_tunables["fPlasmaBeamWeaponBoost"]["value"] = 1.08
+    ridley_tunables["fPlasmaBeamWeaponBoostPhaseDisplacement"]["value"] = 1.08
+    ridley_tunables["fGrabPlasmaBeam"]["value"] = 1.0
+    ridley_tunables["fGrabPlasmaBeamCharge"]["value"] = 1.0
+    ridley_tunables["fGrabPlasmaBeamWeaponBoost"]["value"] = 1.08
+    ridley_tunables["fGrabPlasmaBeamWeaponBoostPhaseDisplacement"]["value"] = 1.08
 
 def apply_static_fixes(editor: PatcherEditor):
     patch_multi_room_gammas(editor)
