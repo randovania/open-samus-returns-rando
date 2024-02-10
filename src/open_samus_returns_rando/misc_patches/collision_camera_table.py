92676c3004ee5053805abca990f3058ce6e7f35b
dyceron <dycerontv@gmail.com>
2024-02-10 17:25:34 -0500

    Fix lua compiling

--- src/open_samus_returns_rando/misc_patches/collision_camera_table.py
+++ src/open_samus_returns_rando/misc_patches/collision_camera_table.py
@@ -1,3 +1,5 @@
+from construct import Container
+from mercury_engine_data_structures.formats.lua import Lua
 from open_samus_returns_rando.misc_patches import lua_util
 from open_samus_returns_rando.patcher_editor import PatcherEditor
 
@@ -5,13 +7,10 @@ from open_samus_returns_rando.patcher_editor import PatcherEditor
 def create_collision_camera_table(editor: PatcherEditor, configuration: dict):
     py_dict: dict = configuration["cosmetic_patches"]["camera_names_dict"]
 
-    file = lua_util.replace_lua_template("cc_to_room_name.lua", { "room_dict" : py_dict}, True).encode("ascii")
-    editor.add_new_asset("system/scripts/cc_to_room_name.lc", file, ["packs/system/system.pkg"])
-
-    # Throws error if done this way
-    # file = lua_util.replace_lua_template("cc_to_room_name.lua", {"room_dict": py_dict}, True)
-    # editor.add_new_asset(
-    #     "system/scripts/cc_to_room_name.lc",
-    #     Lua(Container(lua_text=file), editor.target_game),
-    #     ["packs/system/system.pkg"]
-    # )
+    file = lua_util.replace_lua_template("cc_to_room_name.lua", {"room_dict": py_dict}, True)
+    print(file)
+    editor.add_new_asset(
+        "system/scripts/cc_to_room_name.lc",
+        Lua(Container(lua_text=file), editor.target_game),
+        ["packs/system/system.pkg"]
+    )
