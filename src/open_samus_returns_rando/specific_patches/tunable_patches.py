8a73455dab69864f4e7c26e7f156faa02a58d9b6
dyceron <dycerontv@gmail.com>
2024-07-26 20:43:25 -0400

    Add support for custom damage reduction per suit

--- src/open_samus_returns_rando/specific_patches/tunable_patches.py
+++ src/open_samus_returns_rando/specific_patches/tunable_patches.py
@@ -5,13 +5,12 @@ from open_samus_returns_rando.patcher_editor import PatcherEditor
 def patch_tunables(editor: PatcherEditor, configuration: dict) -> None:
     tunables = editor.get_file("system/tunables/tunables.bmtun", Bmtun)
     _patch_scan_pulse(tunables)
-    _reserves_per_tank(tunables, configuration)
+    _reserves_per_tank(tunables, configuration.get("reserves_per_tank", {}))
+    _suit_reductions(tunables, configuration.get("suit_reductions", {}))
 
 
 def _patch_scan_pulse(tunables: Bmtun) -> None:
-    tunables.raw["classes"]["CTunableAbilityScanningPulse"]["tunables"][
-        "fConsumptionOnActivation"
-    ]["value"] = 0.0
+    tunables.raw["classes"]["CTunableAbilityScanningPulse"]["tunables"]["fConsumptionOnActivation"]["value"] = 0.0
 
 
 def _reserves_per_tank(tunables: Bmtun, configuration: dict) -> None:
@@ -20,3 +19,9 @@ def _reserves_per_tank(tunables: Bmtun, configuration: dict) -> None:
     amiibo["fSETankSize"]["value"] = configuration["aeion_tank_size"]
     amiibo["fMissileTankSize"]["value"] = configuration["missile_tank_size"]
     amiibo["fSuperMissileTankSize"]["value"] = configuration["super_missile_tank_size"]
+
+
+def _suit_reductions(tunables: Bmtun, configuration: dict) -> None:
+    life = tunables.raw["classes"]["Player|CTunablePlayerLifeComponent"]["tunables"]
+    life["fVariaSuitDamageMult"]["value"] = configuration["varia_suit_reduction"]
+    life["fGravitySuitDamageMult"]["value"] = configuration["gravity_suit_reduction"]
