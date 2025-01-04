3ed44cf469e5bc0117df665451098efa091d7016
dyceron <dycerontv@gmail.com>
2025-01-04 11:29:15 -0500

    Revert "Merge pull request #501 from randovania/gawron-groove-patch"
    This reverts commit e7e3ab00f38a54d374cb598a22d5b1228ce50347, reversing
    changes made to 12e8a4c22017ac9ec874d322568d95fee3b350c7.

--- src/open_samus_returns_rando/specific_patches/static_fixes.py
+++ src/open_samus_returns_rando/specific_patches/static_fixes.py
@@ -339,10 +339,6 @@ def disable_vignettes(editor: PatcherEditor) -> None:
         "s040_area4": [
             {"block_group": 27, "names": ["s040_area4_111", "sg_vinegtte_003"]}
         ],
-        # Gawron Groove Gamma Tunnel Outer Power Beam Block
-        "s050_area5": [
-            {"block_group": 14, "names": ["vignette_casca_05"]},
-        ],
         # Diggernaut Bomb Blocks
         "s070_area7": [
             {"block_group": 52, "names": ["casca_vignette_03"]}
@@ -353,10 +349,6 @@ def disable_vignettes(editor: PatcherEditor) -> None:
         ],
     }
     scenario_objects_list: dict[str, list[dict[str, typing.Any]]] = {
-        # Gawron Groove Gamma Tunnel Outer Power Beam Block
-        "s050_area5": [
-            {"indices": [1881]}
-        ],
         # Middle Save Station Water Bomb Blocks
         "s090_area9": [
             {"indices": [1]}
@@ -395,12 +387,6 @@ def remove_a6_lower_dna_seal_gullugg(editor: PatcherEditor) -> None:
     editor.remove_entity({"scenario": "s070_area7", "layer": 4, "actor": "GulluggStr_057"})
 
 
-def remove_a4_cm_gawron_grove_tunnel_block(editor: PatcherEditor) -> None:
-    a4 = editor.get_file("maps/levels/c10_samus/s050_area5/s050_area5.bmsbk", Bmsbk)
-    # Remove the block from the object list
-    a4.raw["collision_cameras"][7]["entries"].pop(3)
-
-
 def apply_static_fixes(editor: PatcherEditor) -> None:
     patch_multi_room_gammas(editor)
     patch_pickup_rotation(editor)
@@ -416,4 +402,3 @@ def apply_static_fixes(editor: PatcherEditor) -> None:
     patch_a1_teleporter_crumbles(editor)
     disable_vignettes(editor)
     remove_a6_lower_dna_seal_gullugg(editor)
-    remove_a4_cm_gawron_grove_tunnel_block(editor)
