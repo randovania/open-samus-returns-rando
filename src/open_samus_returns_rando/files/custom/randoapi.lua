f5ebb20f3aa576c170dbbb30d28119bff58d7ffa
dyceron <dycerontv@gmail.com>
2025-01-21 18:28:57 -0500

    Fix randoapi and cleanup randomizersuit

--- src/open_samus_returns_rando/files/custom/randoapi.lua
+++ src/open_samus_returns_rando/files/custom/randoapi.lua
@@ -4,8 +4,21 @@ RandoApi = RandoApi or {}
 function RandoApi.main()
 end
 
-function RandoApi.ChangeSuitValues(hasVaria, hasGravity)
+function RandoApi.UpdateSuits()
     hasVaria = RandomizerPowerup.GetItemAmount("ITEM_VARIA_SUIT") > 0
     hasGravity = RandomizerPowerup.GetItemAmount("ITEM_GRAVITY_SUIT") > 0
+
+    -- Update damage reductions based on the suits
     RandoApi.ChangeSuitValues(hasVaria, hasGravity)
+
+    -- Model and damage_alarm updates
+    if hasGravity then
+        Game.GetEntity("Samus").MODELUPDATER.sModelAlias = "Gravity"
+        if hasVaria then
+            Game.GetPlayer():StopEntityLoopWithFade("actors/samus/damage_alarm.wav", 0.6)
+        end
+    elseif hasVaria then
+        Game.GetEntity("Samus").MODELUPDATER.sModelAlias = "Varia"
+        Game.GetPlayer():StopEntityLoopWithFade("actors/samus/damage_alarm.wav", 0.6)
+    end
 end
