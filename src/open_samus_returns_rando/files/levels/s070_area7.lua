db4636420f667025b69a47224fa0ec036c780c30
dyceron <38679103+dyceron@users.noreply.github.com>
2025-02-07 07:10:10 -0500

    Remove spaces

--- src/open_samus_returns_rando/files/levels/s070_area7.lua
+++ src/open_samus_returns_rando/files/levels/s070_area7.lua
@@ -166,9 +166,9 @@ end
 function s070_area7.OnEnter_Diggernaut_Access()
   if Init.sFinalBoss == "Diggernaut" then
     if not Blackboard.GetProp("GAME", "OBJECTIVE_COMPLETE") then
-       local startpoint = "Diggernaut" 
-       GUI.LaunchMessage("Not enough Metroid DNA!\nCollect " .. RandomizerPowerup.GetItemAmount(ITEM_ADN) .. " more DNA to fight Diggernaut!", "RandomizerPowerup.Dummy", "") 
-       Game.AddSF(0, "Scenario.FinalBossReload", "s", startpoint)
+      local startpoint = "Diggernaut" 
+      GUI.LaunchMessage("Not enough Metroid DNA!\nCollect " .. RandomizerPowerup.GetItemAmount(ITEM_ADN) .. " more DNA to fight Diggernaut!", "RandomizerPowerup.Dummy", "") 
+      Game.AddSF(0, "Scenario.FinalBossReload", "s", startpoint)
     else
       Game.GetPlayer().vPos = V3D(-18500, -7600, 0)
     end
